7d9cdad7f94f6f33f5ef99f9b5e4a3135d4940bb
Shulika Sergey <developer.shulika@gmail.com>
2022-07-11 01:13:44 +0300

    fix bug pastOrder null

--- lib/src/features/profile/cubit/past_orders_cubit.dart
+++ lib/src/features/profile/cubit/past_orders_cubit.dart
@@ -1,7 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:food_delivery/src/features/cart/data/models/order_item.dart';
 import 'package:food_delivery/src/features/profile/data/repositories/past_orders_repository.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../../../core/errors/flutter_toast_warning.dart';
 
 part 'past_orders_cubit.freezed.dart';
 
@@ -14,12 +18,18 @@ class PastOrdersCubit extends Cubit<PastOrdersState> {
     emit(const PastOrdersState.loading());
 
     try {
-      final data = await repository.fetchOrders();
-      if (data.isEmpty) {
-        emit(const PastOrdersState.initial());
-      } else {
+      final data =
+          await repository.fetchOrders().timeout(const Duration(seconds: 5));
+      if (data.isNotEmpty) {
         emit(PastOrdersState.data(orders: data));
+      } else {
+        emit(const PastOrdersState.initial());
       }
+    } on TimeoutException catch (_) {
+      FlutterToastWarning.showToast(
+          message:
+              'TimeOut Error!!! The server is not responding, check Internet connection!',
+          isError: true);
     } catch (e) {
       emit(const PastOrdersState.initial());
       rethrow;
--- lib/src/features/profile/data/repositories/past_orders_repository.dart
+++ lib/src/features/profile/data/repositories/past_orders_repository.dart
@@ -12,20 +12,22 @@ class PastOrdersRepository {
         .ref("orders/${FirebaseAuth.instance.currentUser?.uid}")
         .orderByValue()
         .get();
-    final extractData =
-        jsonDecode(jsonEncode(response.value)) as Map<String, dynamic>;
-    final tempOrders = extractData.values;
+    if (response.value != null) {
+      final extractData =
+          jsonDecode(jsonEncode(response.value)) as Map<String, dynamic>;
+      final tempOrders = extractData.values;
 
-    for (var order in tempOrders) {
-      final amount = order['amount'] as int;
-      final dateTime = order['dateTime'] as String;
-      final list = order['items'] as List<dynamic>;
-      final items = list.map((e) => CartItem.fromJson(e)).toList();
-      final orderItem = OrderItem(
-          items: items,
-          amount: amount.toDouble(),
-          dateTime: DateTime.parse(dateTime));
-      ordersList.add(orderItem);
+      for (var order in tempOrders) {
+        final amount = order['amount'] as int;
+        final dateTime = order['dateTime'] as String;
+        final list = order['items'] as List<dynamic>;
+        final items = list.map((e) => CartItem.fromJson(e)).toList();
+        final orderItem = OrderItem(
+            items: items,
+            amount: amount.toDouble(),
+            dateTime: DateTime.parse(dateTime));
+        ordersList.add(orderItem);
+      }
     }
 
     return ordersList;
